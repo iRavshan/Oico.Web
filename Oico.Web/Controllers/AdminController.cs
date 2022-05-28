d80062bd275d51f65b1b5e0a5032cf767d888fe8
Ravshan Sadikov <ravshan4ikov@gmail.com>
2022-05-29 01:37:24 +0500

    aaa

--- Oico.Web/Controllers/AdminController.cs
+++ Oico.Web/Controllers/AdminController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Oico.Domain;
 using Oico.Service.Services;
 using Oico.Web.ViewModels.Admin;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -18,30 +20,58 @@ namespace Oico.Web.Controllers
         }
 
         private readonly IProductService productService;
+        private readonly IWebHostEnvironment webHost;
 
-        public AdminController(IProductService productService)
+        public AdminController(IProductService productService,
+                               IWebHostEnvironment webHost)
         {
             this.productService = productService;
+            this.webHost = webHost;
         }
 
         [HttpPost]
         public IActionResult Create(CreateViewModel model)
         {
-            Product product = new Product
+            if (ModelState.IsValid)
             {
-                Id = Guid.NewGuid(),
-                Price = model.Price,
-                WholesalePrice = model.WholesalePrice,
-                Name = model.Name,
-                NumberOfBox = model.NumberOfBox,
-                NumberOfBoxes = model.NumberOfBoxes,
-                ImageUrl = "rasm",
-                Massa = model.Massa
-            };
+                string uniqueFileName = string.Empty;
 
-            productService.Create(product);
+                if(model.Image != null)
+                {
+                    string uploadFolder = Path.Combine(webHost.WebRootPath, "img/product");
+                    uniqueFileName = Guid.NewGuid().ToString() + "_" + model.Image.FileName;
+                    string imageFilePath = Path.Combine(uploadFolder, uniqueFileName);
+                    model.Image.CopyTo(new FileStream(imageFilePath, FileMode.Create));
+                }
+
+                Product product = new Product
+                {
+                    Id = Guid.NewGuid(),
+                    Price = model.Price,
+                    WholesalePrice = model.WholesalePrice,
+                    Name = model.Name,
+                    NumberOfBox = model.NumberOfBox,
+                    NumberOfBoxes = model.NumberOfBoxes,
+                    ImageUrl = uniqueFileName,
+                    Massa = model.Massa
+                };
+
+                productService.Create(product);
+
+                return RedirectToAction("products");
+            }
 
             return View();
+        }
+
+        public async Task<IActionResult> Products()
+        {
+            ProductsViewModel model = new ProductsViewModel
+            {
+                AllProducts = await productService.GetAll()
+            };
+
+            return View(model);
         }
     }
 }
---
+++ Oico.Web/ViewModels/Admin/ProductsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Oico.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Oico.Web.ViewModels.Admin
+{
+    public class ProductsViewModel
+    {
+        public IEnumerable<Product> AllProducts { get; set; }
+    }
+}
