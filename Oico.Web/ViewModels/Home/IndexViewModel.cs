a585862185d57b859acd868cacfcd27399a1cc4a
Ravshan Sadikov <ravshan4ikov@gmail.com>
2022-05-29 17:59:27 +0500

    vavav

---
+++ Oico.Web/ViewModels/Home/IndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Oico.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Oico.Web.ViewModels.Home
+{
+    public class IndexViewModel
+    {
+        public IEnumerable<Product> AllProducts { get; set; }
+        public IEnumerable<Product> LastProducts { get; set; }
+    }
+}
